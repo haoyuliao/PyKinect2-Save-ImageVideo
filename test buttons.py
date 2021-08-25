02c6ec191346ebff6743a5a1d04c5d8a9be48fae
haoyuliao <haoyuliao@ufl.edu>
2021-08-24 23:09:31 -0400

    test

---
+++ test buttons.py
@@ -0,0 +1,14 @@
+import cv2
+
+while True:
+    key = cv2.waitKey(1) 
+    print('test')
+    # press 'q' to exit
+    if key == ord('q'):
+        break
+    elif key == ord('b'):
+        break
+        # change a variable / do something ...
+    elif key == ord('k'):
+        break
+        # change a variable / do something ...
